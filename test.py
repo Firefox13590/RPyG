ccf6edff97ee80860e0d61f8b79a30f0b773a1dd
Firefox13590 <pharlyb@gmail.com>
2025-02-01 20:10:12 -0500

    mvt clavier continu

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ logo = pyglet.resource.image("images/favicon.ico")
 fenetre = pyglet.window.Window()
 fenetre.set_icon(logo)
 
+keys = key.KeyStateHandler()
+fenetre.push_handlers(keys)
+
 
 label = pyglet.text.Label("Hello world!",
                    font_size=16,
@@ -14,22 +17,39 @@ label = pyglet.text.Label("Hello world!",
                    anchor_x="center", anchor_y="center")
 
 
-@fenetre.event
-def on_key_press(symbol, modifiers):
-    if symbol in (key.A, key.LEFT):
-        label.x -= 16
-    if symbol in (key.D, key.RIGHT):
-        label.x += 16
-    if symbol in (key.S, key.DOWN):
-        label.y -= 16
-    if symbol in (key.W, key.UP):
-        label.y += 16
-    pass
+
+
+# @fenetre.event
+# def on_key_press(symbol, modifiers):
+#     if keys[key.A] or keys[key.LEFT]:
+#         print("left")
+#         label.x -= 16
+#     if keys[key.D] or keys[key.RIGHT]:
+#         label.x += 16
+#     if keys[key.S] or keys[key.DOWN]:
+#         label.y -= 16
+#     if keys[key.W] or keys[key.UP]:
+#         label.y += 16
+#     pass
+
 
 @fenetre.event
 def on_draw():
     fenetre.clear()
     label.draw()
+    # fenetre.push_handlers(keys)
+    pass
+
+@fenetre.event
+def on_text_motion(motion):
+    if motion == key.MOTION_LEFT:
+        label.x -= 16
+    if motion == key.MOTION_RIGHT:
+        label.x += 16
+    if motion == key.MOTION_DOWN:
+        label.y -= 16
+    if motion == key.MOTION_UP:
+        label.y += 16
     pass
 
 
