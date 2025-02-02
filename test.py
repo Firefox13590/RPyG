321aff626a3d55766e763753e2835cbc8a26608d
Firefox13590 <pharlyb@gmail.com>
2025-02-01 19:09:06 -0500

    mvt clavier

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import pyglet
+from pyglet.window import key
+
+logo = pyglet.resource.image("images/favicon.ico")
+
 
 fenetre = pyglet.window.Window()
+fenetre.set_icon(logo)
 
 
 label = pyglet.text.Label("Hello world!",
@@ -10,9 +15,22 @@ label = pyglet.text.Label("Hello world!",
 
 
 @fenetre.event
+def on_key_press(symbol, modifiers):
+    if symbol in (key.A, key.LEFT):
+        label.x -= 16
+    if symbol in (key.D, key.RIGHT):
+        label.x += 16
+    if symbol in (key.S, key.DOWN):
+        label.y -= 16
+    if symbol in (key.W, key.UP):
+        label.y += 16
+    pass
+
+@fenetre.event
 def on_draw():
     fenetre.clear()
     label.draw()
+    pass
 
 
 pyglet.app.run()
