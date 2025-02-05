ede8304efdf08dcd3b6b9c232b8d309bff1e126a
Firefox13590 <pharlyb@gmail.com>
2025-02-04 22:35:08 -0500

    formatage

--- test.py
+++ test.py
@@ -10,8 +10,20 @@ fenetre.set_icon(logo)
 keys = key.KeyStateHandler()
 fenetre.push_handlers(keys)
 
+states = ["idle", "move"]
+currentState = states[0]
+idleChar = (" O\n"
+            "[|]\n"
+            " ^\n")
+print(idleChar)
+
 
 label = pyglet.text.Label("Hello world!",
+                   font_size=16,
+                   x=fenetre.width/2, y=fenetre.height/2,
+                   anchor_x="center", anchor_y="center")
+
+character = pyglet.text.Label(currentState,
                    font_size=16,
                    x=fenetre.width/2, y=fenetre.height/2,
                    anchor_x="center", anchor_y="center")
@@ -21,39 +33,76 @@ label = pyglet.text.Label("Hello world!",
 
 @fenetre.event
 def on_key_press(symbol, modifiers):
+    global currentState
+    currentState = states[0]
+
     if keys[key.A] or keys[key.LEFT]:
-        print("left")
-        label.x -= 16
+        character.x -= 16
+        currentState = states[1]
         # key.MOTION_LEFT = LEFT
     if keys[key.D] or keys[key.RIGHT]:
-        print("right")
-        label.x += 16
+        character.x += 16
+        currentState = states[1]
     if keys[key.S] or keys[key.DOWN]:
-        print("down")
-        label.y -= 16
+        character.y -= 16
+        currentState = states[1]
     if keys[key.W] or keys[key.UP]:
-        print("up")
-        label.y += 16
+        character.y += 16
+        currentState = states[1]
+
+    character.text = currentState
     pass
 
-
 @fenetre.event
-def on_draw():
-    fenetre.clear()
-    label.draw()
-    fenetre.push_handlers(keys)
+def on_key_release(symbol, modifiers):
+    global currentState
+    currentState = states[0]
+
+    # if keys[key.A] or keys[key.LEFT]:
+    #     character.x -= 16
+    #     currentState = states[1]
+    #     # key.MOTION_LEFT = LEFT
+    # if keys[key.D] or keys[key.RIGHT]:
+    #     character.x += 16
+    #     currentState = states[1]
+    # if keys[key.S] or keys[key.DOWN]:
+    #     character.y -= 16
+    #     currentState = states[1]
+    # if keys[key.W] or keys[key.UP]:
+    #     character.y += 16
+    #     currentState = states[1]
+
+    character.text = currentState
     pass
 
 @fenetre.event
 def on_text_motion(motion):
+    global currentState
+    currentState = states[0]
+
     if motion == key.MOTION_LEFT:
-        label.x -= 16
+        character.x -= 16
+        currentState = states[1]
+        # character.text = currentState
     if motion == key.MOTION_RIGHT:
-        label.x += 16
+        character.x += 16
+        currentState = states[1]
     if motion == key.MOTION_DOWN:
-        label.y -= 16
+        character.y -= 16
+        currentState = states[1]
     if motion == key.MOTION_UP:
-        label.y += 16
+        character.y += 16
+        currentState = states[1]
+
+    character.text = currentState
+    pass
+
+@fenetre.event
+def on_draw():
+    fenetre.clear()
+    # label.draw()
+    character.draw()
+    fenetre.push_handlers(keys)
     pass
 
 
