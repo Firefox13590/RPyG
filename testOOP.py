db1345a37ce127d993025db15abae00ad48ddfaa
Firefox13590 <pharlyb@gmail.com>
2025-02-06 13:50:49 -0500

    mvt pour instance fenetre

--- testOOP.py
+++ testOOP.py
@@ -1,4 +1,6 @@
 import pyglet
+from pyglet.event import EVENT_HANDLE_STATE
+from pyglet.window import key
 
 
 class Fenetre(pyglet.window.Window):
@@ -9,29 +11,69 @@ class Fenetre(pyglet.window.Window):
         self.alive = 1
 
         self.charText = "O /|\\ /\\"
+        self.charText2 = ""
         self.msgText = "Hello world! We ballin today"
-        self.label = pyglet.text.Label(self.charText,
+        self.states = ["idle", "move"]
+        self.currentState = self.states[0]
+
+        self.label = pyglet.text.Label(self.currentState,
                                        multiline=True, font_size=21,
                                        x=self.width//2, y=self.height//2,
                                        width=10, height=10)
+        pass
+
+    def on_close(self) -> None:
+        self.alive = 0
+        pass
 
     def on_draw(self):
-        self.clear()
-        self.label.draw()
-
-    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+        self.render()
         pass
 
-    def on_mouse_release(self, x: int, y: int, button: int, modifiers: int):
+    def on_key_press(self, symbol: int, modifiers: int) -> EVENT_HANDLE_STATE:
+        mvtKeys = (key.A, key.LEFT, key.D, key.RIGHT, key.S, key.DOWN, key.W, key.UP)
+        if symbol == key.ESCAPE:
+            self.alive = 0
+
+        if symbol in mvtKeys:
+            self.currentState = self.states[1]
+
+        if symbol in mvtKeys[:2]:
+            self.label.x -= 16
+        if symbol in mvtKeys[2:4]:
+            self.label.x += 16
+        if symbol in mvtKeys[4:6]:
+            self.label.y -= 16
+        if symbol in mvtKeys[6:]:
+            self.label.y += 16
+
+        self.label.text = self.currentState
         pass
 
-    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+    def on_key_release(self, symbol: int, modifiers: int) -> EVENT_HANDLE_STATE:
+        self.currentState = self.states[0]
+        self.label.text = self.currentState
         pass
 
     def update(self, dt):
         pass
 
+    def render(self):
+        self.clear()
+
+        self.label.draw()
+
+        self.flip()
+        pass
+
+    def run(self):
+        while self.alive == 1:
+            self.render()
+            event = self.dispatch_events()
+        pass
+
 
 if __name__ == "__main__":
     f = Fenetre()
-    pyglet.app.run()
+    f.run()
+
