42bae84dbf06151ec599d101f79dc7375d2efc40
Firefox13590 <pharlyb@gmail.com>
2025-02-06 12:05:03 -0500

    functioning inherited window

--- testOOP.py
+++ testOOP.py
@@ -1,28 +1,37 @@
 import pyglet
-from pyglet.event import EVENT_HANDLE_STATE
-from pyglet.window import key
-
-logo = pyglet.resource.image("images/favicon.ico")
 
 
 class Fenetre(pyglet.window.Window):
     def __init__(self):
         super().__init__()
 
-        self.set_icon(logo)
+        self.set_icon(pyglet.resource.image("images/favicon.ico"))
         self.alive = 1
 
-    def on_draw(self) -> EVENT_HANDLE_STATE:
-        self.render()
+        self.charText = "O /|\\ /\\"
+        self.msgText = "Hello world! We ballin today"
+        self.label = pyglet.text.Label(self.charText,
+                                       multiline=True, font_size=21,
+                                       x=self.width//2, y=self.height//2,
+                                       width=10, height=10)
 
-    def render(self):
+    def on_draw(self):
         self.clear()
-        self.flip()
+        self.label.draw()
 
-    def run(self):
-        while self.alive == 1:
-            self.render()
-            event = self.dispatch_event()
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+        pass
+
+    def on_mouse_release(self, x: int, y: int, button: int, modifiers: int):
+        pass
+
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+        pass
+
+    def update(self, dt):
+        pass
 
 
-# Fenetre.run(Fenetre)
+if __name__ == "__main__":
+    f = Fenetre()
+    pyglet.app.run()
